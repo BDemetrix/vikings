205fc1e31faf03bcc30755adbc54da29e926625d
Bogdanov Dmitry <b.demetrix@gmail.com>
2020-10-10 10:54:08 +0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,31 @@
+$(function(){
+
+    $('.menu__btn').on('click', function() {
+        $('.menu__list').toggleClass('menu__list--activ');
+    })
+
+    /*
+    $('[data-fancybox]').fancybox({
+        youtube: {
+            controls: 0,
+            showinfo: 0
+        }
+    });
+    */
+
+    $('.heroes__slider-img').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        asNavFor: '.heroes__slider-text',
+        prevArrow: '<button class="slick-prev slick-btn"><img src="img/arrow_left.png" alt="prev"></button>',
+        nextArrow: '<button class="slick-next slick-btn"><img src="img/arrow_right.png" alt="next"></button>'
+    });
+    $('.heroes__slider-text').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        asNavFor: '.heroes__slider-img',
+        fade: true,
+        arrows: false
+    });
+
+});
