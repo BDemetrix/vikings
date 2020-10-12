075dad55f1d19653bc04f1015819bb2f48a00443
Bogdanov Dmitry <b.demetrix@gmail.com>
2020-10-12 15:47:05 +0300

    Fixed adaptive of 'heroes' section

--- js/main.js
+++ js/main.js
@@ -19,9 +19,22 @@ $(function(){
     $('.heroes__slider-img').slick({
         slidesToShow: 1,
         slidesToScroll: 1,
+        adaptiveHeight: true,
         asNavFor: '.heroes__slider-text',
         prevArrow: '<button class="slick-prev slick-btn"><img src="img/arrow_left.png" alt="prev"></button>',
-        nextArrow: '<button class="slick-next slick-btn"><img src="img/arrow_right.png" alt="next"></button>'
+        nextArrow: '<button class="slick-next slick-btn"><img src="img/arrow_right.png" alt="next"></button>',
+        responsive: [
+            {
+                breakpoint: 671,
+                settings: {
+                    arrows: false,
+                    dots: true,
+                    dotsClass: 'heroes__dots',
+                    infinite: true,
+                    autoplay: true,
+                }
+            }
+        ]  
     });
     $('.heroes__slider-text').slick({
         slidesToShow: 1,
