0052a92255f8f6c6f8fad573f1dd2187022307d1
Bogdanov Dmitry <b.demetrix@gmail.com>
2020-10-12 22:03:17 +0300

    Fixed adaptive of 'season-1'

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
 $(function(){
 
     $('.menu__btn').on('click', function() {
+        if ($('.season-info')) {
+            $('.season-info').toggleClass('season-info--activ');
+            $('.page - logo').toggleClass('page-logo--activ')
+            $('.menu').toggleClass('menu--activ');
+            $('.menu__list').toggleClass('menu__list--activ');
+            return false;
+        }
         $('.header').toggleClass('header--activ');
         $('.logo').toggleClass('logo--activ');
         $('.menu').toggleClass('menu--activ');
@@ -20,6 +27,8 @@ $(function(){
         slidesToShow: 1,
         slidesToScroll: 1,
         adaptiveHeight: true,
+        infinite: true,
+        lazyLoad: 'progressive',
         asNavFor: '.heroes__slider-text',
         prevArrow: '<button class="slick-prev slick-btn"><img src="img/arrow_left.png" alt="prev"></button>',
         nextArrow: '<button class="slick-next slick-btn"><img src="img/arrow_right.png" alt="next"></button>',
@@ -27,6 +36,7 @@ $(function(){
             {
                 breakpoint: 671,
                 settings: {
+                    lazyLoad: 'progressive',
                     arrows: false,
                     dots: true,
                     dotsClass: 'heroes__dots',
